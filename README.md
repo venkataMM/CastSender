4036818ae9aaffd7911a511da4d7734a4610d579
Venkata Sai Krishna <venkata.krishna@mediamelon.com>
2023-01-26 17:50:11 +0530

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# CastSender
+# CastVideos-chrome (reference Chrome sender app)
+
+This Google Cast demo app shows how to cast videos from a Chrome browser using Cast Chrome Sender SDK. It serves to demonstrate how to write a HTML5/Javascript Cast sender app that is fully compliant with the Design Checklist.
+
+**This is a reference sender app to be used as the starting point for your Chrome sender app**
+
+[List of reference apps and tutorials](https://developers.google.com/cast/docs/downloads)
+
+## Setup Instructions
+1. Get a Google Cast device and get it set up for development: https://developers.google.com/cast/docs/developers#setup_for_development.
+1. [Optional] Register an application on the Developers Console [http://cast.google.com/publish](http://cast.google.com/publish "Google Cast Developer Console").
+  The easiest would be to use the Styled Media Receiver option. You will get an App ID when you finish registering your application. This project uses a
+  published Application ID that can be used to run the app without using your own ID but if you need to do any console debugging, you would need to have your own ID.
+1. Install the latest Chrome browser.
+1. Put all files on your own server.
+1. Use the default media receiver app or change YOUR_APP_ID to your own in CastVideos.js.
+1. Open a browser and point to your page at http://[YOUR_SERVER_LOCATION]/CastVideos-chrome/.
+
+## Documentation
+* [Google Cast Chrome Sender Overview](https://developers.google.com/cast/docs/chrome_sender/)
+* [Developer Guides](https://developers.google.com/cast/docs/developers)
+
+## References
+* [Chrome Sender Reference](http://developers.google.com/cast/docs/reference/chrome)
+* [Design Checklist](http://developers.google.com/cast/docs/design_checklist)
+
+## How to report bugs
+* [Google Cast SDK Support](https://developers.google.com/cast/support)
+* For sample app issues, open an issue on this GitHub repo.
+
+## Contributions
+Please read and follow the steps in the [CONTRIBUTING.md](CONTRIBUTING.md).
+
+## License
+See [LICENSE](LICENSE).
+
+## Terms
+Your use of this sample is subject to, and by using or downloading the sample files you agree to comply with, the [Google APIs Terms of Service](https://developers.google.com/terms/) and the [Google Cast SDK Additional Developer Terms of Service](https://developers.google.com/cast/docs/terms/).
---
+++ media.js
@@ -0,0 +1,98 @@
+// Copyright 2021 Google LLC. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+/**
+ * Hardcoded media json objects
+ */
+let mediaJSON = {
+  'media': [{
+      'subtitle': 'Grumpy Bunny is grumpy',
+      'contentUrl': 'big_buck_bunny/prog/big_buck_bunny_prog.mp4',
+      'contentType': 'video/mp4',
+      'thumb': 'big_buck_bunny/images/screenshot1.png',
+      'title': 'Big Buck Bunny',
+      'duration': 596
+    },
+    {
+      'subtitle': 'Friends Proog and Emo journey inside the folds of a seemingly infinite Machine.',
+      'contentUrl': 'elephants_dream/prog/elephants_dream_prog.mp4',
+      'contentType': 'video/mp4',
+      'thumb': 'elephants_dream/images/screenshot7.png',
+      'title': 'Elephant Dream',
+      'duration': 653
+    },
+    {
+      'subtitle': 'Introducing Chromecast. The easiest way to enjoy online video and music on your TV. Find out more at google.com/chromecast.',
+      'contentUrl': 'ads/for_bigger_blazes/prog/for_bigger_blazes_prog.mp4',
+      'contentType': 'video/mp4',
+      'thumb': 'ads/for_bigger_blazes/images/screenshot8.png',
+      'title': 'For Bigger Blazes',
+      'duration': 15
+    },
+    {
+      'subtitle': 'Introducing Chromecast. The easiest way to enjoy online video and music on your TV. Find out more at google.com/chromecast.',
+      'contentUrl': 'ads/for_bigger_escapes/prog/for_bigger_escapes_prog.mp4',
+      'contentType': 'video/mp4',
+      'thumb': 'ads/for_bigger_escapes/images/screenshot7.png',
+      'title': 'For Bigger Escape',
+      'duration': 15
+    },
+    {
+      'subtitle': 'Introducing Chromecast. The easiest way to enjoy online video and music on your TV. Find out more at google.com/chromecast.',
+      'contentUrl': 'ads/for_bigger_fun/prog/for_bigger_fun_prog.mp4',
+      'contentType': 'video/mp4',
+      'thumb': 'ads/for_bigger_escapes/images/screenshot7.png',
+      'title': 'For Bigger Fun',
+      'duration': 60
+    },
+    {
+      'subtitle': 'Introducing Chromecast. The easiest way to enjoy online video and music on your TV. Find out more at google.com/chromecast.',
+      'contentUrl': 'ads/for_bigger_joyrides/prog/for_bigger_joyrides_prog.mp4',
+      'contentType': 'video/mp4',
+      'thumb': 'ads/for_bigger_joyrides/images/screenshot8.png',
+      'title': 'For Bigger Joyrides',
+      'duration': 15
+    },
+    {
+      'subtitle': 'Introducing Chromecast. The easiest way to enjoy online video and music on your TV. Find out more at google.com/chromecast.',
+      'contentUrl': 'ads/for_bigger_meltdowns/prog/for_bigger_meltdowns_prog.mp4',
+      'contentType': 'video/mp4',
+      'thumb': 'ads/for_bigger_meltdowns/images/screenshot6.png',
+      'title': 'For Bigger Meltdowns',
+      'duration': 15
+    },
+    {
+      'subtitle': 'A lonely young woman, Sintel, helps and befriends a dragon, whom she calls Scales. But when he is kidnapped by an adult dragon, Sintel decides to embark on a dangerous quest to find her lost friend Scales.',
+      'contentUrl': 'sintel/prog/sintel_prog.mp4',
+      'contentType': 'video/mp4',
+      'thumb': 'sintel/images/screenshot7.png',
+      'title': 'Sintel',
+      'duration': 887
+    },
+    {
+      'subtitle': 'In an apocalyptic future, a group of soldiers and scientists takes refuge in Amsterdam to try to stop an army of robots that threatens the planet.',
+      'contentUrl': 'tears_of_steel/prog/tears_of_steel_prog.mp4',
+      'contentType': 'video/mp4',
+      'thumb': 'tears_of_steel/images/screenshot7.png',
+      'title': 'Tears of Steel',
+      'duration': 743
+    }
+  ]
+};
+
+export {
+  mediaJSON
+}
